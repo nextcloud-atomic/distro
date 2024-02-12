50d11771575b50889b3969285f27e9ac3c817bac
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2024-02-12 11:35:43 +0100

    Enable ipv6 for docker and use custom apache port

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -2,6 +2,7 @@
 
 set -e
 docker stop skiff && docker rm skiff || true
+sudo rm -rf skiffos/workspaces/virt/docker-run/docker-persist
 make compile
 make cmd/virt/docker/buildimage
 make cmd/virt/docker/run
